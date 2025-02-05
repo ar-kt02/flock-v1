4184aecdb87b25c4e71b2f11e136db5786800d64
A Katz <akatz@MacBook-Pro.local>
2025-02-05 03:33:07 +0000

    chore: register JWT plugin to main app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import Fastify from "fastify";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import userRoutes from "./modules/user/user.route";
+import jwtPlugin from "./plugins/jwt";
 
 const fastify = Fastify({
    logger: true,
 }).withTypeProvider<TypeBoxTypeProvider>();
 
 async function main() {
+   await fastify.register(jwtPlugin);
    fastify.register(userRoutes, { prefix: "/api/users" });
 
    try {
