96e2034d0e15c6b2083500223a233f9d17abe61d
A Katz <akatz@MacBook-Pro.local>
2025-02-05 03:35:53 +0000

    feat: created login endpoint/route, user login auth with JWT and protected route

--- backend/src/modules/user/user.route.ts
+++ backend/src/modules/user/user.route.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { Type } from "@fastify/type-provider-typebox";
-import { createUser, findUserByEmail } from "./user.service";
+import { comparePassword, createUser, findUserByEmail } from "./user.service";
 
 async function userRoutes(fastify: FastifyInstance) {
    fastify.post(
@@ -49,6 +49,71 @@ async function userRoutes(fastify: FastifyInstance) {
                .status(500)
                .send({ message: "Failed to register account." });
          }
+      }
+   );
+
+   fastify.post(
+      "/login",
+      {
+         schema: {
+            body: Type.Object({
+               email: Type.String({ format: "email" }),
+               password: Type.String(),
+            }),
+            response: {
+               200: Type.Object({ token: Type.String() }),
+               401: Type.Object({ message: Type.String() }),
+               500: Type.Object({ message: Type.String() }),
+            },
+         },
+      },
+      async (request, reply) => {
+         const { email, password } = request.body as {
+            email: string;
+            password: string;
+         };
+
+         try {
+            const user = await findUserByEmail(email);
+
+            if (!user) {
+               return reply
+                  .status(401)
+                  .send({ message: "Invalid credentials." });
+            }
+
+            const validatePassword = await comparePassword(
+               password,
+               user.password
+            );
+
+            if (!validatePassword) {
+               return reply
+                  .status(401)
+                  .send({ message: "Invalid credentials." });
+            }
+
+            const token = fastify.jwt.sign({ id: user.id, email: user.email });
+            return reply.status(200).send({ token });
+         } catch (error) {
+            console.log("login route", error);
+            return reply.status(500).send({ message: "Failed to login." });
+         }
+      }
+   );
+
+   fastify.get(
+      "/protected",
+      {
+         schema: {
+            response: {
+               200: Type.Object({ message: Type.String() }),
+            },
+         },
+         onRequest: [fastify.authenticate],
+      },
+      async (request, reply) => {
+         return { message: "Successfully accessed protetced route." };
       }
    );
 }
