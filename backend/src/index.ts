44d92236c569c0c19275fc7c06580b29f1b12422
A Katz <akatz@MacBook-Pro.local>
2025-02-07 15:20:35 +0000

    refactor: allow dyanmic PORT setting and added schema validation for env setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,22 +2,19 @@ import Fastify from "fastify";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import userRoutes from "./modules/user/user.route";
 import jwtPlugin from "./plugins/jwt";
-import dotenv from "dotenv";
-
-const envFile = `.env.${process.env.NODE_ENV || "development"}`;
-
-dotenv.config({ path: envFile });
+import { config } from "./config/index";
 
 const fastify = Fastify({
    logger: true,
 }).withTypeProvider<TypeBoxTypeProvider>();
 
 async function main() {
-   await fastify.register(jwtPlugin);
+   await fastify.register(jwtPlugin, { secret: config.JWT_SECRET });
    fastify.register(userRoutes, { prefix: "/api/users" });
 
    try {
-      await fastify.listen({ port: 3000 });
+      const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+      await fastify.listen({ port: port });
    } catch (err) {
       fastify.log.error(err);
       process.exit(1);
