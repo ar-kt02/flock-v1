7efeae09cf9f7dbe9c5d8c40c15cbaa6792b80b1
A Katz <akatz@mac.home>
2025-02-16 22:06:45 +0100

    update jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,7 +2,10 @@
 module.exports = {
    preset: "ts-jest",
    testEnvironment: "node",
-   testPathIgnorePatterns: ["<rootDir>/src/config"],
+   testPathIgnorePatterns: [
+      "<rootDir>/src/config",
+      "<rootDir>/__tests__/test-utils.ts",
+   ],
    setupFilesAfterEnv: ["<rootDir>/src/config/test.ts"],
    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
    transform: {
