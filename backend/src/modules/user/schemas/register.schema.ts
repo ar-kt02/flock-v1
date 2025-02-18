ffe0a75e496112bdf7e68b7a72cfc3c9c6d9ba5e
A Katz <akatz@mac.home>
2025-02-18 14:23:26 +0100

    fix: prevent public user from creating accounts with priviledged roles through api

--- backend/src/modules/user/schemas/register.schema.ts
+++ backend/src/modules/user/schemas/register.schema.ts
@@ -1,29 +1,29 @@
 import { Type } from "@fastify/type-provider-typebox";
 
 export const RegisterBodySchema = Type.Object({
-   email: Type.String({ format: "email" }),
-   password: Type.String({ minLength: 8 }),
+  email: Type.String({ format: "email" }),
+  password: Type.String({ minLength: 8 }),
 });
 
 export const RegisterResponseSchema = {
-   201: Type.Object({
-      id: Type.String(),
-      email: Type.String({ format: "email" }),
-   }),
-   400: Type.Object({
-      message: Type.String(),
-   }),
-   500: Type.Object({
-      message: Type.String(),
-   }),
+  201: Type.Object({
+    id: Type.String(),
+    email: Type.String({ format: "email" }),
+  }),
+  400: Type.Object({
+    message: Type.String(),
+  }),
+  500: Type.Object({
+    message: Type.String(),
+  }),
 };
 
 export const CreateAdminBodySchema = Type.Object({
-   email: Type.String({ format: "email" }),
-   password: Type.String({ minLength: 8 }),
-   role: Type.Enum({
-      ATTENDEE: "ATTENDEE",
-      ORGANIZER: "ORGANIZER",
-      ADMIN: "ADMIN",
-   }),
+  email: Type.String({ format: "email" }),
+  password: Type.String({ minLength: 8 }),
+  role: Type.Enum({
+    ATTENDEE: "ATTENDEE",
+    ORGANIZER: "ORGANIZER",
+    ADMIN: "ADMIN",
+  }),
 });
--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -5,45 +5,45 @@ const prisma = new PrismaClient();
 const saltRounds = 12;
 
 export const createUser = async (
-   email: string,
-   password: string,
-   role: UserRole = UserRole.ATTENDEE
+  email: string,
+  password: string,
+  role: UserRole = UserRole.ATTENDEE,
 ) => {
-   try {
-      const hashedPassword = await bcrypt.hash(password, saltRounds);
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-      const user = await prisma.user.create({
-         data: {
-            email: email,
-            password: hashedPassword,
-            role: role,
-         },
-      });
+    const user = await prisma.user.create({
+      data: {
+        email: email,
+        password: hashedPassword,
+        role: role,
+      },
+    });
 
-      return user;
-   } catch (error) {
-      throw new Error("Sign up failed.");
-   }
+    return user;
+  } catch (error) {
+    throw new Error("Sign up failed.");
+  }
 };
 
 export const findUserByEmail = async (email: string) => {
-   try {
-      const user = await prisma.user.findUnique({
-         where: {
-            email,
-         },
-      });
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-      return user;
-   } catch (error) {
-      throw new Error("Cannot find user with email.");
-   }
+    return user;
+  } catch (error) {
+    throw new Error("Cannot find user with email.");
+  }
 };
 
 export const comparePassword = async (password: string, hashed: string) => {
-   try {
-      return await bcrypt.compare(password, hashed);
-   } catch (error) {
-      throw new Error("Failed to validate password.");
-   }
+  try {
+    return await bcrypt.compare(password, hashed);
+  } catch (error) {
+    throw new Error("Failed to validate password.");
+  }
 };
