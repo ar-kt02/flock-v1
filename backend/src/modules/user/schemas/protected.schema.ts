bfdb9c55328e761cdef4cdb1657d6e82969ad7f9
A Katz <akatz@MacBook-Pro-4.local>
2025-03-02 01:11:10 +0000

    feat: created logout route with jwt blacklisting. re-factor: temporarily use protected route for role validation on frontend, removed duplicate fastify instance

--- backend/src/modules/user/schemas/protected.schema.ts
+++ backend/src/modules/user/schemas/protected.schema.ts
@@ -1,5 +1,7 @@
 import { Type } from "@fastify/type-provider-typebox";
 
 export const ProtectedResponseSchema = {
-  200: Type.Object({ message: Type.String() }),
+  200: Type.Object({ role: Type.String() }),
+  500: Type.Object({ message: Type.String() }),
+  401: Type.Object({ message: Type.String() }),
 };
