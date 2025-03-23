ccd5804a729a97d50a552396080e0f2df4272d75
A Katz <akatz@mac.home>
2025-03-23 21:24:24 +0100

    create Profile types

---
+++ frontend/src/types/profile.ts
@@ -0,0 +1,11 @@
+interface Profile {
+  userId: string;
+  email: string;
+  firstName: string;
+  surname: string;
+  phoneNumber: string;
+  location: string;
+  interests: string[];
+}
+
+export default Profile;
